867129ed30bcd33c9ccc8c4016b768412dc53118
Risto Leivo <leivo.risto@gmail.com>
2022-10-21 14:34:21 +0300

    add playbackrate calibration

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -17,37 +17,44 @@ let leftHeelRepeated = 0
 let rightToeRepeated = 0
 let leftToeRepeated = 0
 
+let rightPrevToeR = 0
+let leftPrevToeR = 0
+
 let rightLegForward = false
 let leftLegForward = false
 
 let rightPrevToeX = 0
 let leftPrevToeX = 0
 
+
+// let rightPrev2ToeX = 0
+// let leftPrevT2oeX = 0
+
 let rightStep = false
 let leftStep = false
 
 let calibrated = false
+let mediapipeCalibrated = false
+
+let startTime, prevTime
+let calibrationTick = 0
+let lastCalibrationTick
 
 function Home() {
     const [file, setFile] = useState(null)
-    const [fileDataURL, setFileDataURL] = useState(null)
+    //const [fileDataURL, setFileDataURL] = useState(null) //poista?
     const [videoSrc, setVideoSrc] = useState(null)
     const [poseTest, setPose] = useState(null)
     // const [firstRun, setFirstRun] = useState(true)
     // const canvasRef = useRef(null)
     const videoRef = useRef(null)
 
-    const handleGo = () => {
-        setVideoSrc(fileDataURL)
-    }
-
-    //const canvasCtx = document.getElementsByClassName('output_canvas')[0]
-
     function onResults(results) {
         //console.log(results)
         if (results.poseLandmarks) {
             if (!calibrated) {
                 if (results.poseWorldLandmarks[30].visibility > 0.2) {
+                    startTime = Date.now()
                     rightPrevHeelX = results.poseWorldLandmarks[30].x
                     rightPrevToeX = results.poseWorldLandmarks[32].x
                     leftPrevHeelX = results.poseWorldLandmarks[29].x
@@ -58,112 +65,92 @@ function Home() {
                     return
                 }
             }
+            
+            if (calibrated && !mediapipeCalibrated) {   
+                videoRef.current.loop = true             
+                calibrationTick++
+                if (Date.now() - startTime > 999) {
+                    if (calibrationTick < 101 && videoRef.current.playbackRate > 0.2) {
+                        if (videoRef.current.playbackRate > 0.2) {
+                            videoRef.current.playbackRate = videoRef.current.playbackRate - 0.05
+                            console.log("slowing down")
+                            console.log(videoRef.current.playbackRate)
+                            console.log(calibrationTick)
+                        }
+                        calibrationTick = 0
+                        startTime = Date.now()
+                    } else if (calibrationTick > 99 || videoRef.current.playbackRate < 0.25) {       
+                        console.log("playbackspeed calibrated:")
+                        console.log(videoRef.current.playbackRate)                 
+                        mediapipeCalibrated = true
+                        videoRef.current.currentTime = 0
+                        videoRef.current.loop = false
+                    }
+                }
+            }
 
-            if (calibrated) {
-                if (rightLegForward && rightHeelRepeated < -2) {
-                    console.log("Heel strike")
-                }
-                if (rightHeelRepeated > 2) {
+            if (calibrated && mediapipeCalibrated) {
+                //console.log('f')               
+
+                if (rightToeRepeated > 1) {
                     rightLegForward = true
-                    rightHeelRepeated = 0
+                    console.log("Right f")
+                    rightToeRepeated = 0
                 }
-                if (rightHeelRepeated < -2) {
+                if (rightToeRepeated < -1) {
+                    if (rightLegForward) {
+                        // console.log("Right heel strike")
+                    }
                     rightLegForward = false
-                    rightHeelRepeated = 0
+                    rightToeRepeated = 0
                 }
-                if (results.poseWorldLandmarks[30].x > rightPrevHeelX) {
-                    rightHeelRepeated++
+                if (leftToeRepeated > 1) {
+                    leftLegForward = true
+                    console.log("Left f")
+                    leftToeRepeated = 0
+                }
+                if (leftToeRepeated < -1) {
+                    leftLegForward = false
+                    leftToeRepeated = 0
+                }
+                if (results.poseWorldLandmarks[32].x > rightPrevToeX) {
+                    rightToeRepeated++
                 } else {
-                    rightHeelRepeated--
+                    rightToeRepeated--
                 }
-                if (results.poseWorldLandmarks[29].x > leftPrevHeelX) {
-                    leftHeelRepeated++
+                if (results.poseWorldLandmarks[31].x > leftPrevToeX) {
+                    leftToeRepeated++
                 } else {
-                    leftHeelRepeated--
+                    leftToeRepeated--
                 }
-                rightPrevHeelX = results.poseWorldLandmarks[30].x
+
+                rightPrevHeelX = results.poseWorldLandmarks[32].x
                 rightPrevToeX = results.poseWorldLandmarks[32].x
                 leftPrevHeelX = results.poseWorldLandmarks[29].x
                 leftPrevToeX = results.poseWorldLandmarks[31].x
             }
         }
 
-        /*
-                    const canvasElement = canvasRef.current
-                    const canvasCtx = canvasElement.getContext("2d")
-                    canvasCtx.save()
-                    canvasCtx.clearRect(0, 0, 1280, 720)
-                    canvasCtx.translate(720, 0)
-                    canvasCtx.scale(-1, 1)
-                    canvasCtx.font = "40px Verdana"
-                    canvasCtx.fillStyle = "#bdffff"
-                    canvasCtx.drawImage(
-                        results.image,
-                        0,
-                        0,
-                        canvasCtx.width,
-                        canvasCtx.height
-                    )
-        
-                    const leftLeg = [
-                        results.poseLandmarks[24],
-                        results.poseLandmarks[23],
-                        results.poseLandmarks[25],
-                        results.poseLandmarks[27],
-                    ]
-        
-                    drawConnectors(canvasCtx, leftLeg, POSE_CONNECTIONS, {
-                        color: "#77bdff",
-                        lineWidth: 4,
-                    })
-                    drawLandmarks(canvasCtx, leftLeg, {
-                        color: "#bd77ff",
-                        lineWidth: 2,
-                    })
-        */
 
-        //videoElement.play()
-        //videoRef.current.pause()
         return;
-        //}
+
     }
 
     const changeHandler = (e) => {
 
 
         const file = e.target.files[0];
-        // if (!file.type.match(imageMimeType)) {
-        //   alert("Image mime type is not valid")
-        //   return
-        // }        
+
         setFile(file)
 
         videoRef.current = document.getElementsByClassName('input_video')[0]
 
         async function onFrame() {
             if (!videoRef.current.ended) {
-                videoRef.current.play()
-                // if (firstRun) {
-                //     console.log("first frame")
-                //     //setFirstRun(false)
-                //     firstRun = false
-                //     videoRef.current.pause()
-                //     setTimeout(onFrame, 1000)
-                // }
 
                 await poseTest.send({
                     image: videoRef.current
                 })
-                //videoRef.current.pause()
-
-
-                // console.log('frame')
-                // console.log(testArray.length)
-
-                // await pose.send({
-                //     image: videoRef.current
-                //   })
-                // https://stackoverflow.com/questions/65144038/how-to-use-requestanimationframe-with-promise    
                 await new Promise(requestAnimationFrame)
                 onFrame()
             }
@@ -173,33 +160,13 @@ function Home() {
 
 
         videoRef.current.onloadeddata = (evt) => {
-            //let video = evt.target
-
-            //console.log(video)
-            //canvasElement.width = video.videoWidth
-            //canvasElement.height = video.videoHeight
-
-            //canvasRef.current.width = video.videoWidth
-            //canvasRef.current.height = video.videoHeight
-
-            //const aspect = video.videoHeight / video.videoWidth
-            //let width, height
-            //if (window.innerWidth > window.innerHeight) {
-            //    height = window.innerHeight
-            //   width = height / aspect
-            // }
-            // else {
-            //    width = window.innerWidth
-            //     height = width * aspect
-            // }
-            videoRef.current.playbackRate = 0.2
+            videoRef.current.playbackRate = 1
             videoRef.current.play()
             onFrame()
         }
     }
 
     useEffect(() => {
-        console.log("pose")
         const pose = new Pose({
             locateFile: (file) => {
                 return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`
@@ -218,18 +185,15 @@ function Home() {
         setPose(pose)
         pose.onResults(onResults)
 
-    }, [])
-
-    useEffect(() => {
-
         let fileReader, isCancel = false
         if (file) {
             fileReader = new FileReader()
             fileReader.onload = (e) => {
                 const { result } = e.target
-                if (result && !isCancel) { // calibrated = false tänne?
+                if (result && !isCancel) {
+                    calibrated = false
                     setVideoSrc(result)
-                    setFileDataURL(result)
+                    //setFileDataURL(result)
                 }
             }
             fileReader.readAsDataURL(file)
@@ -250,25 +214,21 @@ function Home() {
 
         <>
             <div className="container">
-                <video className="input_video" src={videoSrc} type='video/mp4' muted="muted" width="480" height="270"></video>
+                <video className="input_video" src={videoSrc} type='video/mp4' muted="muted" width="960" height="540"></video>
                 {/* <div className="canvas-container">
                     <canvas ref={canvasRef} className="output_canvas" width="1280px" height="720px">
                     </canvas>
                 </div> */}
                 <form>
-                    <p>
-                        <label htmlFor='video'>Select video</label>
+                    <p style={{ display: file ? 'none' : 'block' }}>
                         <input
                             type="file"
                             id='video'
-                            accept='.mp4, .ogg'
+                            accept='.mp4, .ogg, .webm'
                             onChange={changeHandler}
                         />
                     </p>
                 </form>
-                {fileDataURL ?
-                    <button type="submit" onClick={handleGo}>Go</button>
-                    : null}
             </div>
         </>
     )
